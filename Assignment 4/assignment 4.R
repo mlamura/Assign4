6b7104a280a552c913f1addd89b23624c0fdf49f
mlamura <35226825+mlamura@users.noreply.github.com>
2018-02-04 21:08:33 -0500

    Assign4 code
    HW4

---
+++ Assignment 4/assignment 4.R
@@ -0,0 +1,11 @@
+hist(bloodp,
+     main="Histogram for Medical Exam",
+     xlab=frequency,
+     border = "blue",
+     col = "green",
+     xlim = c(40,110),
+    las=0,
+     breaks =10)
+
+
+boxplot(bloodp,frequency)
